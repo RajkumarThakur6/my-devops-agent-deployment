25b34cd9346bacd3dd6bc95e0f0ebf2491b3536e
Raj <rajkumar.thakur6@gmail.com>
2025-08-03 23:25:11 +0530

    feat: Generate terraform file for deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,15 +5,15 @@ provider "aws" {
 }
 
 # Terraform resource block to define an AWS EC2 instance
-resource "aws_instance" "my-new-app" {
+resource "aws_instance" "my-web-server" {
   # Amazon Machine Image (AMI) ID. This one is for Ubuntu 20.04 LTS in ap-south-1.
   # Ensure this AMI is available in your selected AWS region.
-  ami           = "ami-0c55b159cbfafe1f0" 
+  ami           = "ami-0f918f7e67a3323f0" 
   instance_type = "t3.micro"
 
   # Tags are key-value pairs that help in managing and identifying resources.
   tags = {
-    Name = "my-new-app"
+    Name = "my-web-server"
     ManagedBy = "LangChainAgent"
   }
 }
