6341976490a6af98347c739994894e67af2b4c43
Raj <rajkumar.thakur6@gmail.com>
2025-08-03 23:00:17 +0530

    fix: Update to a valid AWS AMI ID again

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "my-new-app" {
   # Amazon Machine Image (AMI) ID. This one is for Ubuntu 20.04 LTS in ap-south-1.
   # Ensure this AMI is available in your selected AWS region.
   ami           = "ami-0c55b159cbfafe1f0" 
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   # Tags are key-value pairs that help in managing and identifying resources.
   tags = {
